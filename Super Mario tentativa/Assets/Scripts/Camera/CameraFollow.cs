cc9a3e7e5d5f57c12c515e5384b5fddde974f6d7
Arthur Palladino <arthur.palladino1@gmail.com>
2025-09-23 18:52:39 -0300

    feat: add jump

--- Super Mario tentativa/Assets/Scripts/Camera/CameraFollow.cs
+++ Super Mario tentativa/Assets/Scripts/Camera/CameraFollow.cs
@@ -5,25 +5,32 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] GameObject target;
     [SerializeField] GameObject map;
     float lastTargetX;
-    Renderer renderer;
+    Renderer rd;
 
-    public float minX;
-    public float maxX;
+    float minX;
+    float maxX;
+
 
     void Start()
     {
-        renderer= map.GetComponent<Renderer>();
+        rd = map.GetComponent<Renderer>();
+        lastTargetX = target.transform.position.x;
     }
 
     void LateUpdate()
     {
         Vector3 curPos = transform.position;
         Vector3 targetPos = target.transform.position;
-        float halfWidth = Camera.main.orthographicSize * Camera.main.aspect;
-        minX = renderer.bounds.min.x + halfWidth;
-        maxX = renderer.bounds.max.x - halfWidth;
-        float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
-        transform.position = new Vector3(clampedX, curPos.y, curPos.z);
+        if (targetPos.x > lastTargetX)
+        {
+            lastTargetX = targetPos.x;
+            float halfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+            minX = rd.bounds.min.x + halfWidth;
+            maxX = rd.bounds.max.x - halfWidth;
+            float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
+            transform.position = new Vector3(clampedX, curPos.y, curPos.z);
+        }
+        
     }
 
 
--- Super Mario tentativa/Assets/Scripts/PlayableChar/MovementScript.cs
+++ Super Mario tentativa/Assets/Scripts/PlayableChar/MovementScript.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    Renderer rd;
+    Rigidbody2D rb;
 
+    bool Jumping = false;
+    void Start()
+    {
+        rd = gameObject.GetComponent<Renderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         Move();
@@ -13,19 +22,39 @@ public class MovementScript : MonoBehaviour
     void Move()
     {
         var playerInput = Input.GetAxis("Horizontal");
+
         if (playerInput != 0)
         {
-            Vector3 moveDirection = new Vector3(playerInput, 0, 0) * moveSpeed * Time.deltaTime;
-            transform.position += moveDirection;
+            float halfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+            //Mantém atualizando o spriteSize para futuras atualizações de sprites, como aumento de personagem ou Yoshi.
+            float spriteSize = rd.bounds.size.x / 2;
+            float minX = Camera.main.transform.position.x - halfWidth + spriteSize;
+            float maxX = Camera.main.transform.position.x + halfWidth - spriteSize;
+            float xMove = playerInput * moveSpeed * Time.deltaTime;
+            float clampedX = Mathf.Clamp(transform.position.x+xMove, minX, maxX);
+            transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+            
         }
         else
         {
-            
+
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Jumping = false;
         }
     }
 
     void Jump()
     {
-        
+        var playerInput = Input.GetAxis("Vertical");
+        if ((playerInput > 0 || Input.GetKey(KeyCode.Space)) && !Jumping)
+        {
+            Jumping = true;
+            rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+        }
     }
 }
